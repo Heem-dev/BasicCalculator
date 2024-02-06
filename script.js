2ed291c9d3d55424779aebdb8f6c7453cbe8bb28
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-02-06 19:07:18 +0300

    Cleared the code. used functions whenever possible

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// select operators.
+// Select operators.
 const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 const add = document.getElementById("add");
@@ -8,194 +8,138 @@ const clear = document.getElementById("clear");
 const decimal = document.getElementById("decimal");
 const clearOne = document.getElementById("clearOne");
 
-// select numbers.
+// Select numbers.
 const numbers = document.querySelectorAll(".number");
 
-// select result.
+// Select result.
 const result = document.getElementById("result");
 
 let currentOperator = "";
 let operatorClicked = false;
-let decimalClicked = false;
 
-// add event listeners to numbers.
-numbers.forEach(number => {
-    number.addEventListener("click", () => {
-        result.value += number.textContent;
-    });
+// Add event listeners to numbers.
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    result.value += number.textContent;
+  });
 });
 
-// add event listeners to operators.
+// Add event listeners to operators.
+function handleOperatorClick(operator, symbol) {
+  if (result.value !== "" && !operatorClicked) {
+    currentOperator = operator;
+    operatorClicked = true;
+    result.value += symbol;
+  }
+}
+
 multiply.addEventListener("click", () => {
-    if (result.value !== "" && !operatorClicked) {
-        currentOperator = "*";
-        operatorClicked = true;
-        result.value += multiply.textContent;
-    }
+  handleOperatorClick("*", multiply.textContent);
 });
 
 divide.addEventListener("click", () => {
-    if (result.value !== "" && !operatorClicked) {
-        currentOperator = "/";
-        operatorClicked = true;
-        result.value += divide.textContent;
-    }
+  handleOperatorClick("/", divide.textContent);
 });
 
 add.addEventListener("click", () => {
-    if (result.value !== "" && !operatorClicked) {
-        currentOperator = "+";
-        operatorClicked = true;
-        result.value += add.textContent;
-    }
+  handleOperatorClick("+", add.textContent);
 });
 
 subtract.addEventListener("click", () => {
-    if (result.value !== "" && !operatorClicked) {
-        currentOperator = "-";
-        operatorClicked = true;
-        result.value += subtract.textContent;
-    }
+  handleOperatorClick("-", subtract.textContent);
 });
 
-// add event listener to equals.
+// Add event listener to equals.
 equals.addEventListener("click", () => {
-    if (result.value !== "") {
+  if (result.value !== "") {
+    const numbers = result.value.split(currentOperator);
+    const number1 = Number(numbers[0]);
+    const number2 = Number(numbers[1]);
 
-
-        let numbers = result.value.split(currentOperator);
-        // console.log(numbers);
-        let number1 = Number(numbers[0]);
-        let number2 = Number(numbers[1]);
-
-        switch (currentOperator) {
-            case "*":
-                answer = number1 * number2;
-                break;
-            case "/":
-                answer = number1 / number2;
-                break;
-            case "+":
-                answer = number1 + number2;
-                break;
-            case "-":
-                answer = number1 - number2;
-                break;
-        }
-
-        
-        result.value = answer;
-        operatorClicked = false;
-        currentOperator = "";
-
+    let answer;
+    switch (currentOperator) {
+      case "*":
+        answer = number1 * number2;
+        break;
+      case "/":
+        answer = number1 / number2;
+        break;
+      case "+":
+        answer = number1 + number2;
+        break;
+      case "-":
+        answer = number1 - number2;
+        break;
     }
+
+    result.value = answer;
+    operatorClicked = false;
+    currentOperator = "";
+  }
 });
 
-// add event listener to clear.
+// Add event listener to clear.
 clear.addEventListener("click", () => {
+  result.value = "";
+  operatorClicked = false;
+  currentOperator = "";
+});
+
+// Add event listener to decimal.
+decimal.addEventListener("click", () => {
+  if (result.value !== "") {
+    result.value += decimal.textContent;
+  }
+});
+
+// Add event listener to clearOne.
+clearOne.addEventListener("click", () => {
+  if (result.value !== "") {
+    const lastDigit = result.value[result.value.length - 1];
+
+    // Check if last digit is an operator.
+    if (["*", "/", "+", "-"].includes(lastDigit)) {
+      currentOperator = "";
+      operatorClicked = false;
+    }
+
+    // Remove last digit.
+    result.value = result.value.slice(0, -1);
+  }
+});
+
+// Add event listeners to keyboard.
+document.addEventListener("keydown", (e) => {
+  if (e.key >= 0 && e.key <= 9) {
+    result.value += e.key;
+  } else if (["*", "/", "+", "-"].includes(e.key)) {
+    if (result.value !== "" && !operatorClicked) {
+      currentOperator = e.key;
+      operatorClicked = true;
+      result.value += e.key;
+    }
+  } else if (e.key === ".") {
+    if (result.value !== "") {
+      result.value += e.key;
+    }
+  } else if (e.key === "Backspace") {
+    if (result.value !== "") {
+      const lastDigit = result.value[result.value.length - 1];
+
+      // Check if last digit is an operator.
+      if (["*", "/", "+", "-"].includes(lastDigit)) {
+        currentOperator = "";
+        operatorClicked = false;
+      }
+
+      // Remove last digit.
+      result.value = result.value.slice(0, -1);
+    }
+  } else if (e.key === "c" || e.key === "C" || e.key === "Delete") {
     result.value = "";
     operatorClicked = false;
     currentOperator = "";
-
-});
-
-// add event listener to decimal.
-decimal.addEventListener("click", () => {
-    if (result.value !== "") {
-        result.value += decimal.textContent;
-        // decimalClicked = true;
-    }
-});
-
-// add event listener to clearOne.
-clearOne.addEventListener("click", () => {
-    if (result.value !== "") {
-        let lastDigit = result.value[result.value.length - 1];
-        // console.log(lastDigit);
-
-        // check if last digit is an operator.
-        if (lastDigit === "*" || lastDigit === "/" || lastDigit === "+" || lastDigit === "-") {
-            currentOperator = "";
-            operatorClicked = false;
-
-            // remove last digit.
-            result.value = result.value.slice(0, -1);
-        }
-        else {
-            // remove last digit.
-            result.value = result.value.slice(0, -1);
-        }
-
-    }
-});
-
-// add event listener to keyboard for numbers.
-document.addEventListener("keydown", (e) => {
-    if (e.key >= 0 && e.key <= 9) {
-        result.value += e.key;
-    }
-});
-
-// add event listener to keyboard for operators.
-document.addEventListener("keydown", (e) => {
-    if (e.key === "*" || e.key === "/" || e.key === "+" || e.key === "-") {
-        if (result.value !== "" && !operatorClicked) {
-            currentOperator = e.key;
-            operatorClicked = true;
-            result.value += e.key;
-        }
-    }
-});
-
-        // add event listener to backspace key for clearOne.
-document.addEventListener("keydown", (e) => { 
-    if (e.key === "Backspace") {
-        if (result.value !== "") {
-            let lastDigit = result.value[result.value.length - 1];
-            // console.log(lastDigit);
-
-            // check if last digit is an operator.
-            if (lastDigit === "*" || lastDigit === "/" || lastDigit === "+" || lastDigit === "-") {
-                currentOperator = "";
-                operatorClicked = false;
-
-                // remove last digit.
-                result.value = result.value.slice(0, -1);
-            }
-            else {
-                // remove last digit.
-                result.value = result.value.slice(0, -1);
-
-            }
-
-        }
-    }
-});
-
-// add event listener to keyboard for decimal.
-document.addEventListener("keydown", (e) => {
-    if (e.key === ".") {
-        if (result.value !== "") {
-            result.value += e.key;
-
-        }
-    }
-});
-
-// add event listener to keyboard for clear.
-document.addEventListener("keydown", (e) => {
-    if (e.key === "c" || e.key === "C" || e.key === "Delete") {
-        result.value = "";
-        operatorClicked = false;
-        currentOperator = "";
-
-    }
-});
-
-// make equal button clickable with enter key.
-document.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        equals.click();
-        
-    }
+  } else if (e.key === "Enter") {
+    equals.click();
+  }
 });
